e4f7f160c38ab70a8903c6dce2caabcdb5d47216
enriqueizel2 <eleaoizel@gmail.com>
2020-05-06 21:17:37 -0400

    first commit

--- js/api.js
+++ js/api.js
@@ -1,11 +1,7 @@
 const url = "https://api.sheety.co/30b6e400-9023-4a15-8e6c-16aa4e3b1e72";
 
 function getRegisters() {
-  fetch(url)
-    .then(function (response) {
-      return response.json();
-    })
-    .catch(function (error) {
-      console.error(error);
-    });
+  const response = fetch(url);
+  const data = response.json();
+  return data;
 }
