44e60e7777a58ff7c2197149b9e535b6c0671271
enriqueizel2 <eleaoizel@gmail.com>
2020-05-10 14:27:36 -0400

    feat(js/index.js):  criando funções para formatar nome e preço

--- js/index.js
+++ js/index.js
@@ -2,11 +2,10 @@ const url = "https://api.sheety.co/30b6e400-9023-4a15-8e6c-16aa4e3b1e72";
 
 const cards = document.querySelector("#cards");
 
-console.log(cards);
-
 async function getRegisters() {
   const response = await fetch(url);
   const data = response.json();
+  console.log(data);
   return data;
 }
 
@@ -16,7 +15,7 @@ function renderCards(cardData) {
 }
 
 function renderCard({ photo, property_type, name, price }) {
-  const div = document.createElement("div");
+  const div = createDiv();
 
   div.className = "card";
 
@@ -27,9 +26,9 @@ function renderCard({ photo, property_type, name, price }) {
     />
     <div class="cardDescription">
       <h3 class="typeProperty">${property_type}</h3>
-      <h2 class="cardName">${name}</h2>
-      <p class="price">R$ ${price}</p>
-    </div
+      <h2 class="cardName">${formatName(name)}</h2>
+      <p class="price">R$ ${formatPrice(price)}</p>
+    </div>
   `;
 
   cards.appendChild(div);
@@ -38,13 +37,25 @@ function renderCard({ photo, property_type, name, price }) {
 // TODO criar os estilos para mensagem de erro
 function renderError() {
   cards.innerHTML = "";
-  const div = document.createElement("div");
+  const div = createDiv();
 
   div.innerHTML = `
     <span>aaa</span>
   `;
 
   cards.appendChild(div);
+}
+
+function createDiv() {
+  return document.createElement("div");
+}
+
+function formatName(name) {
+  return name.toLowerCase();
+}
+
+function formatPrice(price) {
+  return parseFloat(price).toFixed(2).replace(".", ",");
 }
 
 async function main() {
